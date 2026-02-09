fe8a9027fae2b09e1732940d546d6e903c5d3808
Mijael Juy <sebastianmijaeljuy@gmail.com>
2026-02-09 18:59:07 -0500

    Arreglar conexion SSL Neon

--- src/config/db.ts
+++ src/config/db.ts
@@ -3,16 +3,33 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-const db = new Sequelize(
-    process.env.DB_NAME || 'sillar_db',
-    process.env.DB_USER || 'postgres',
-    process.env.DB_PASS || 'admin1234',
-    {
-        host: process.env.DB_HOST || 'localhost',
+let db: Sequelize;
+
+if (process.env.DATABASE_URL) {
+    // MODO PRODUCCIÓN (RENDER / NEON)
+    db = new Sequelize(process.env.DATABASE_URL, {
         dialect: 'postgres',
-        logging: false,
-        port: 5432,
-    }
-);
+        protocol: 'postgres',
+        dialectOptions: {
+            ssl: {
+                require: true,
+                rejectUnauthorized: false
+            }
+        },
+        logging: false
+    });
+} else {
+    db = new Sequelize(
+        process.env.DB_NAME || 'sillar_db',
+        process.env.DB_USER || 'postgres',
+        process.env.DB_PASS || 'admin1234',
+        {
+            host: process.env.DB_HOST || 'localhost',
+            dialect: 'postgres',
+            logging: false,
+            port: 5432,
+        }
+    );
+}
 
 export default db;
